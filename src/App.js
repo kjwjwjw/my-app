9879907fa74fc512b6eb51b32b83ae72289c9f15
kjwjwjw <tbs03112@naver.com>
2024-04-24 20:47:07 +0900

    Counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
+
 import Home from "./pages/Home"
+import About from "./pages/About"
+import Counter from "./pages/Counter"
+import Input from "./pages/Input"
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <nav>
+      <Link to="/">Home</Link> | 
+      <Link to="/about">About</Link> | 
+      <Link to="/Counter">Counter</Link>
+      <Link to="/Input">Input</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Counter" element={<Counter />} />
+        <Route path="/Input" element={<Input />} />
+      </Routes>
+      
+
     </div>
   );
 }
